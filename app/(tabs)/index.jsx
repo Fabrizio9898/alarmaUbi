ce1b7b2b21d37bb1dafde61a050caca64c1740fd
Fabrizio9898 <fabrizioandrader9898@gmail.com>
2025-07-25 15:37:10 -0300

    map view ready

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,18 +1,31 @@
-import {  StyleSheet } from 'react-native';
-import SearchBar from '../../components/SearchBar.component';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import SearchBar from '../../components/SearchBar.component';
+import Map from '../../components/MapView.component';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView style={styles.container}>
-           <SearchBar />
-
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Map />
+      
+      <SafeAreaView style={styles.safeOverlay} edges={['top']}>
+        <SearchBar />
+      </SafeAreaView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+    position: 'relative',
+  },
+  safeOverlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1, // para que esté encima del mapa
+    paddingHorizontal: 16,
+  },
 });
--- components/MapView.component.jsx
+++ components/MapView.component.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, View } from 'react-native';
 export default function Map() {
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={StyleSheet.absoluteFill} />
     </View>
   );
 }
@@ -13,9 +13,5 @@ export default function Map() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
   },
 });
